bde1ee2a8250f5502f5e230f271864549c1a708e
suflistico <ppanguinao.p@gmail.com>
2023-06-18 23:21:46 -0400

    desafio React-II

---
+++ src/Views/Home.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {    
+    
+        return (
+            <div className="mt-5">
+            <h1>Consumo de APi mediante el uso de useNavigate</h1>
+            
+            </div>
+        );
+}
